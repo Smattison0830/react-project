d7a889e69ea28ffff25087146afa9eebedd85904
Spencer Mattison <spenceramattison@gmail.com>
2021-10-13 10:23:22 +0900

    created details page and started on router

---
+++ src/Details.js
@@ -0,0 +1,19 @@
+import { Component } from "react";
+import { withRouter } from "react-router-dom";
+
+class Details extends Component {
+    constructor () {
+        super();
+
+        this.state = { loading : true};
+    } 
+    async componentDidMount () {
+
+    }
+    render() {
+        return <h2>HIIIIIIII</h2>
+    }
+}
+
+export default Details;
+ 
--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,16 +1,39 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Results from "./Results";
+import useBreedList from "./useBreedList";
 
-const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+const ANIMALS = ["Bird", "Cat", "Dog", "Rabbit", "Reptile"];
 
 const SearchParams = () => {
-  const [location, setLocation] = useState("Seattle, WA");
+  const [location, setLocation] = useState("");
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
-  const breeds = [];
+  const [pets, setPets] = useState([]);
+  const [breeds] = useBreedList(animal.toLowerCase());
+
+  console.log(breeds);
+
+  useEffect(() => {
+    requestPets();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  async function requestPets() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+    );
+    const json = await res.json();
+
+    setPets(json.pets);
+  }
 
   return (
     <div className="search-params">
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           location
           <input
@@ -54,6 +77,7 @@ const SearchParams = () => {
         </label>
         <button>Submit</button>
       </form>
+      <Results pets={pets} />        
     </div>
   );
 };
